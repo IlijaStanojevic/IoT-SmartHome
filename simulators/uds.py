ccb30e0da90ab0d80555fcd22df921aa29acc223
Marko Milijanovic <markomilijanovic01@gmail.com>
2023-11-11 18:47:29 +0100

    Added dus1 with simulator

---
+++ simulators/uds.py
@@ -0,0 +1,28 @@
+import time
+import random
+
+def generate_values(initial_distance=200):
+    distance = initial_distance
+    person_detected = False
+    countdown = random.randint(1, 3)
+    while True:
+        # distance = random.randint(1, 200)
+        if not person_detected:
+            distance = 200
+        if person_detected:
+            distance = random.randint(1, 50)
+            if countdown == 0:
+                person_detected = False
+            countdown -= 1
+        elif random.randint(1, 100) <= 10:
+            person_detected = True
+            countdown = random.randint(1, 3)
+        yield distance
+
+
+def run_dus_simulator(delay, callback, stop_event):
+    for d in generate_values():
+        time.sleep(delay)  # Delay between readings (adjust as needed)
+        callback(d)
+        if stop_event.is_set():
+            break
