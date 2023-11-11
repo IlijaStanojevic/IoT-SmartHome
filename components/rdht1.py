5db4ae7f50e1ff11ba857d8f9a69b510f3a1da5d
Ilija Stanojevic <ilija.stanojevic123@gmail.com>
2023-11-11 19:45:22 +0100

    Reformat: add rdht2

--- components/rdht1.py
+++ components/rdht1.py
@@ -4,11 +4,12 @@ import threading
 import time
 
 
-def dht_callback(humidity, temperature, code):
+def dht_callback(humidity, temperature, code, device):
     t = time.localtime()
     OutputLock.safe_print("="*20)
     OutputLock.safe_print(f"Timestamp: {time.strftime('%H:%M:%S', t)}")
     OutputLock.safe_print(f"Code: {code}")
+    OutputLock.safe_print(f"Device: {device}")
     OutputLock.safe_print(f"Humidity: {humidity}%")
     OutputLock.safe_print(f"Temperature: {temperature}°C")
     OutputLock.safe_print("=" * 20)
@@ -16,11 +17,11 @@ def dht_callback(humidity, temperature, code):
 
 def run_rdht1(settings, threads, stop_event):
         if settings['simulated']:
-            print("Starting rdth1 sumilator")
-            rdht1_thread = threading.Thread(target = run_dht_simulator, args=(2, dht_callback, stop_event))
+            print("Starting rdth1 simulator")
+            rdht1_thread = threading.Thread(target = run_dht_simulator, args=(2, dht_callback, stop_event, "RDHT1"))
             rdht1_thread.start()
             threads.append(rdht1_thread)
-            print("RDht1 sumilator started")
+            print("RDht1 simulator started")
         else:
             from sensors.dht import run_dht_loop, DHT
             print("Starting rdht1 loop")
