1370965df8dabfa2d8dfcbb2f600b8183da40786
Ilija Stanojevic <ilija.stanojevic123@gmail.com>
2024-01-24 20:55:07 +0100

    Add rgb controls

--- iot-smarthome-front/src/components/controllers.jsx
+++ iot-smarthome-front/src/components/controllers.jsx
@@ -1,19 +1,34 @@
 import logo from "../logo.svg";
 import {Stack} from "@mui/material";
+import {Label} from "@mui/icons-material";
 
 
 const Controllers = () => {
     return (
         <div className="controllers">
-            <div className="rgb">
-                <Stack
-                >
+            <Stack>
+                <div className="rgb">
+                    <h2>RGB</h2>
+                    <Stack>
+                        <div className="row">
+                            <button>Off</button>
+                            <button style={{ backgroundColor: 'WHITE' }}>WHITE</button>
+                            <button style={{ backgroundColor: 'RED' }}>RED</button>
+                            <button style={{ backgroundColor: 'GREEN' }}>GREEN</button>
+                        </div>
+                        <div className="row">
+                            <button style={{ backgroundColor: 'BLUE' }}>BLUE</button>
+                            <button style={{ backgroundColor: 'YELLOW' }}>YELLOW</button>
+                            <button style={{ backgroundColor: 'PURPLE' }}>PURPLE</button>
+                            <button style={{ backgroundColor: 'LIGHT_BLUE' }}>LIGHT_BLUE</button>
+                        </div>
+                    </Stack>
+                </div>
+                <div className="alarm-clock">
 
-                </Stack>
-            </div>
-            <div className="alarm-clock">
+                </div>
+            </Stack>
 
-            </div>
 
         </div>
     );
